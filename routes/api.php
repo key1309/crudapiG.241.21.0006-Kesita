f0cfa6ca71916034ed508b2de476cf4c961ba8fa
Saputra-003 <tawakalsv@gmail.com>
2022-12-04 18:24:49 +0700

    add auth sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\RegisterController;
 use App\Http\Controllers\MahasiswaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,7 +20,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
+
 Route::post('mahasiswa/create', [MahasiswaController::class, 'store']);
 Route::put('mahasiswa/update', [MahasiswaController::class, 'update']);
 Route::get('mahasiswa/read/{nim}', [MahasiswaController::class, 'show']);
 Route::delete('mahasiswa/delete/{nim}', [MahasiswaController::class, 'destroy']);
+//
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('mahasiswa/create2', [MahasiswaController::class, 'store']);
+    Route::put('mahasiswa/update2', [MahasiswaController::class, 'update']);
+    Route::get('mahasiswa/read2/{nim}', [MahasiswaController::class, 'show']);
+    Route::delete('mahasiswa/delete2/{nim}', [MahasiswaController::class, 'destroy']);
+});
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', function () {
+    return response('Anda tidak diizinkan!');
+})->name('login');
